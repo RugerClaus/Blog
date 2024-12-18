65f41e03fb03b324b93f941e20483239a30a7818
rugerclaus <rogerrock12games@gmail.com>
2024-12-18 14:33:22 -0700

    modded about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,10 @@
 @extends('main')
 @section('main')
     <h1>About</h1>
-    <img src="{{asset('images/me.jpeg')}}" alt="Picture of me - Credit Reina Meza">
+    <figure>
+        <img src="{{asset('images/me.jpeg')}}" alt="Picture of me - Credit Reina Meza">
+    </figure>
+    <caption>
+        <p>This is a picture of me. I'm just a dude.</p>
+    </caption>
 @endsection
