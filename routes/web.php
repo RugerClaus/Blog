5fcbae757a76d45e1afa976f2fd3dda9787922aa
rugerclaus <rogerrock12games@gmail.com>
2024-12-18 14:13:26 -0700

    fixed routes again

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
 
-Route::get('/', PagesController::class, 'home');
-Route::get('/blog', PagesController::class, 'blog');
-Route::get('/contact', PagesController::class, 'contact');
-Route::get('/about', PagesController::class, 'about');
+Route::get('/', [PagesController::class, 'home']);
+Route::get('/blog', [PagesController::class, 'blog']);
+Route::get('/contact', [PagesController::class, 'contact']);
+Route::get('/about', [PagesController::class, 'about']);
