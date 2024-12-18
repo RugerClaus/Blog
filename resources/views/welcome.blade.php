60567de5236a03b59e607ac41a3e95a2045eadcd
rugerclaus <rogerrock12games@gmail.com>
2024-12-18 13:41:25 -0700

    initial styles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('css/main.css')}}">
-    <title>Document</title>
+    <title>Unknown Anarchist: @yield('title')</title>
 </head>
 <body>
-    <h1>Hello, World</h1>
+    <header>
+        <h1>Unknown Anarchist</h1>
+        <nav>
+            <a href="/">Home</a>
+            <a href="/blog">Blog</a>
+            <a href="/contact">Contact</a>
+            <a href="/about">About</a>
+        </nav>
+    </header>
 </body>
 </html>
