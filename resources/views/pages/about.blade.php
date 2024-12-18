5481875072fd6f4a34a78eeee3c048d60151a1a1
rugerclaus <rogerrock12games@gmail.com>
2024-12-18 14:52:28 -0700

    modded about page again

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,10 +1,15 @@
 @extends('main')
 @section('main')
-    <h1>About</h1>
+<section class="about">
+    <h1>About Me</h1>
     <figure>
         <img src="{{asset('images/me.webp')}}" alt="Picture of me - Credit Reina Meza">
     </figure>
     <caption>
-        <p>This is a picture of me. I'm just a dude.</p>
+        <p>My name is Roger. I'm somewhere in my late 20s and live in the less mormon section of Utah known as Salt Lake City. I've lived in this valley
+            long enough - IMO at least. This blog is where I talk about my interests and opinions. I'm an anarchist, and prefer to leave most things to chance.
+            We'll cross that bridge when we come to it.
+        </p>
     </caption>
+</section>
 @endsection
