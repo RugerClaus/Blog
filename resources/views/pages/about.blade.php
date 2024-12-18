eae412d8561f046892f3f152473584a320fd14ee
rugerclaus <rogerrock12games@gmail.com>
2024-12-18 14:36:44 -0700

    modded about page image

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,7 +2,7 @@
 @section('main')
     <h1>About</h1>
     <figure>
-        <img src="{{asset('images/me.jpeg')}}" alt="Picture of me - Credit Reina Meza">
+        <img src="{{asset('images/me.webp')}}" alt="Picture of me - Credit Reina Meza">
     </figure>
     <caption>
         <p>This is a picture of me. I'm just a dude.</p>
