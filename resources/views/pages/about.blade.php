55b31329283f4b50a0c6abde64a13bbe8ddae0e0
rugerclaus <rogerrock12games@gmail.com>
2024-12-19 00:30:05 -0700

    blurb

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -6,7 +6,7 @@
         <img src="{{asset('images/me.webp')}}" alt="Picture of me - Credit Reina Meza">
     </figure>
     <caption>
-        <p>My name is Roger. I'm somewhere in my late 20s and live in the less mormon section of Utah known as Salt Lake City. I've lived in this valley
+        <p class="blurb">My name is Roger. I'm somewhere in my late 20s and live in the less mormon section of Utah known as Salt Lake City. I've lived in this valley
             long enough - IMO at least. This blog is where I talk about my interests and opinions. I'm an anarchist, and prefer to leave most things to chance.
             We'll cross that bridge when we come to it.
         </p>
