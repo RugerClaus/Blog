85cc4af482222aa585d8b92d80de48f601489078
rugerclaus <rogerrock12games@gmail.com>
2024-12-21 10:30:14 -0700

    adjusting line breaks

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -9,11 +9,11 @@
         <p class="blurb">My name is Roger. I'm somewhere in my late 20s and live in the less mormon section of Utah known as Salt Lake City. I've lived in this valley
             long enough - IMO at least. This blog is where I talk about my interests and opinions. I'm an anarchist, and prefer to leave most things to chance.
             We'll cross that bridge when we come to it.
-            <br>
+            <br />
             The irony of this page is of course not lost on me. The picture clearly stabs the anonymity of a real Unknown Anarchist right in the ass. This itself is a statement.
             Why reveal my face on a pro-anonymity blog? Because I can, and damn the consequences. This is just another way to determine my true friends who will stand in class solidarity.
             No. This site is a protest against having to use my anonymity. People have been killed for less, and that's precisely the point of this page.
-            <br>
+            <br />
             So read on, dear... reader, and share if you dare.
         </p>
     </caption>
