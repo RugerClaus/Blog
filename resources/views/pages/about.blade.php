7ae1a29730b6cf17e63acc21fa191558e202e752
rugerclaus <rogerrock12games@gmail.com>
2024-12-21 10:28:04 -0700

    about blurb

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -9,7 +9,12 @@
         <p class="blurb">My name is Roger. I'm somewhere in my late 20s and live in the less mormon section of Utah known as Salt Lake City. I've lived in this valley
             long enough - IMO at least. This blog is where I talk about my interests and opinions. I'm an anarchist, and prefer to leave most things to chance.
             We'll cross that bridge when we come to it.
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro dolore, praesentium veniam distinctio cupiditate eligendi iusto velit optio ea perferendis sequi voluptatibus culpa fugiat nobis quos, obcaecati tempore quas sunt dolores neque, molestiae delectus ad saepe. Ex pariatur, accusantium debitis id ipsam corrupti tempora minus ad sunt libero quia sed itaque corporis rerum unde nihil culpa, earum voluptates asperiores voluptate voluptatem illo, vitae praesentium. Amet accusamus eveniet adipisci reprehenderit sit corrupti? Ut culpa, repellat perferendis cum nesciunt quam fugit adipisci incidunt aut ratione aliquam eos saepe cumque, quia deleniti, accusamus vel maiores recusandae temporibus ipsa et nemo. Non corrupti culpa autem at architecto expedita inventore ratione eveniet vitae atque unde eius delectus ea perferendis modi, ut, temporibus numquam. Odio perferendis rem quidem nulla numquam voluptate voluptatem saepe, quisquam officiis nam aut eum incidunt id amet commodi maxime unde quaerat nisi voluptatum expedita temporibus recusandae! Ea ex quae suscipit quod adipisci reprehenderit facere repellat, quam veniam voluptatibus molestiae! Sapiente, commodi voluptatum! Velit nam laudantium consectetur fuga labore natus explicabo animi voluptatem maxime officia blanditiis eum, error ducimus accusamus odit? Laborum sapiente quae id cupiditate debitis sint aliquid quia mollitia quaerat libero voluptates voluptas nemo eum pariatur beatae, quo asperiores vero eveniet.
+            <br>
+            The irony of this page is of course not lost on me. The picture clearly stabs the anonymity of a real Unknown Anarchist right in the ass. This itself is a statement.
+            Why reveal my face on a pro-anonymity blog? Because I can, and damn the consequences. This is just another way to determine my true friends who will stand in class solidarity.
+            No. This site is a protest against having to use my anonymity. People have been killed for less, and that's precisely the point of this page.
+            <br>
+            So read on, dear... reader, and share if you dare.
         </p>
     </caption>
 </section>
