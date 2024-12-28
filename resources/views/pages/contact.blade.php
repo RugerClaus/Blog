8638ef7bb3fb472010a16829732d27fd4bf90596
rugerclaus <rogerrock12games@gmail.com>
2024-12-28 10:05:52 -0700

    validator 2

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -4,18 +4,18 @@
     <h1 class="pageHead">Contact Me</h1>
     <section class="contact">
         <h4>This is the best way to contact me. Perks of developing my own webapp, I don't have to give you my email.</h4>
-        <form action="/send" method="post" class="messageForm">
-            @csrf
+        <div class="messageForm">
             <label for="subject">Subject: </label>
             <input type="text" name="subject" id="subject">
             <label for="message">Message: </label>
             <textarea name="message" id="message" cols="30" rows="10"></textarea>
             <button id="send" onclick="validate()">Send</button>
-        </form>
+        </div>
     </section>
     <div class="validator">
-        <div class="question">
+        <form action="/send" method="post" class="question">
             What is {{$num1}} {{$operator}} {{$num2}}
+            <input type="hidden" name="messageSubject">
             <input type="text" name="answer" id="answer" class="answer">
             <input type="submit" value="Submit" id="validate">
         </div>
@@ -25,10 +25,12 @@
         const answer = document.getElementById('answer')
         const validate = document.getElementById('validate')
         const question = document.querySelector('.question')
-        const submit = document.getElementById('send') 
-        function validate(){
+        const submit = document.getElementById('send')
+        const form  = document.querySelector('form')
+        form.addEventListener('sumbit', (e) => {
+            event.preventDefault()
             validator.style.display = 'block'
             question.style.display = 'flex'
-        }
+        })
     </script>
 @endsection
