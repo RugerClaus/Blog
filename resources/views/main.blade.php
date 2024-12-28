f80c1d62581a77031fba4ab5c1bb083686ecaf90
rugerclaus <rogerrock12games@gmail.com>
2024-12-27 20:29:40 -0700

    icon?

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('css/main.css')}}">
-    <title>Unknown Anarchist: @yield('title')</title>
+    <link rel="shortcut icon" href="{{asset('images/favicon.ico')}}" type="image/x-icon">
+    <title>UA: @yield('title')</title>
 </head>
 <body>
     <nav>
