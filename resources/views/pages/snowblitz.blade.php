f3f69a9916e00fd1c5b349c66020c91a30d92e3c
rugerclaus <rogerrock12games@gmail.com>
2025-05-08 13:31:52 -0600

    added images

--- resources/views/pages/snowblitz.blade.php
+++ resources/views/pages/snowblitz.blade.php
@@ -10,7 +10,7 @@
         <caption>Main menu of game</caption>
         <br>
         <figure>
-            <img src="images/snowblitz_game" alt="Gameplay screenshot of Snow Blitz">
+            <img src="images/snowblitz_game.png" alt="Gameplay screenshot of Snow Blitz">
         </figure>
 
         <p>
