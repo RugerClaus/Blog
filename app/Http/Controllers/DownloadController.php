86259698d7a17663c0e51a13435d3c2ec9cd278f
rugerclaus <rogerrock12games@gmail.com>
2025-05-08 19:27:41 -0600

    test

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -10,7 +10,8 @@ class DownloadController extends Controller
     public function SnowBlitz($filename)
     {
         $path = public_path("downloads/{$filename}");
-
+        \Log::info("Trying to download: $filename");
+        dd($filename); // this should stop and show the filename
         if (!file_exists($path)) {
             \Log::error("File not found: " . $path);
             abort(404);
