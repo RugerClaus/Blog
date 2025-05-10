2e761867fc8d33293d52fa5064ed181db1055500
rugerclaus <rogerrock12games@gmail.com>
2025-05-10 17:40:20 -0600

    micro updates

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,10 +4,16 @@
     <h1 class="pageHead">Home</h1>
     <section class="homeText">
         <h2>Contribute to Snow Blitz</h2>
-        <p>Hello and welcome to my website. If you'd like to submit a pull request, please do! I always welcome contributors. </p>
-        <p>You can do a pull request for Snow Blitz or this website but I am only taking help for documenting Snow Blitz for this site</p>
+            <section class="homeSubText">
+                <p>Hello and welcome to my website. If you'd like to submit a pull request, please do! I always welcome contributors. </p>
+                <p>You can do a pull request for Snow Blitz or this website but I am only taking help for documenting Snow Blitz for this site</p>
+            </section>
         <h2>MODDERS</h2>
-        <p>I hate closed source bullshit, so you can do what you would like. Feel free to create your own fork of Snow Blitz</p>
-        <p>If you want support on the actual game source, I'll be spinning up unknownanarchist.blog's catchall email or something.</p>
+
+        <section class="homeSubText">
+            <p>I hate closed source bullshit, so you can do what you would like. Feel free to create your own fork of Snow Blitz</p>
+            <p>If you want support on the actual game source, I'll be spinning up unknownanarchist.blog's catchall email or something.</p>
+        </section>
+
     </section>
 @endsection
