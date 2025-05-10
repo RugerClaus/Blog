69d2192a605a2818b040e84fb6a0af859fdf1a0e
rugerclaus <rogerrock12games@gmail.com>
2025-05-10 17:44:37 -0600

    micro updates

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,12 +6,15 @@
         <h2>Contribute to Snow Blitz</h2>
             <section class="homeSubText">
                 <p>Hello and welcome to my website. If you'd like to submit a pull request, please do! I always welcome contributors. </p>
+                <a href="https://github.com/rugerclaus">My Github</a>
+                <p>Public repos can be found there. Feel free to fork any repo. The sequencer is a fork from another repo that I have yet to do anything with</p>
                 <p>You can do a pull request for Snow Blitz or this website but I am only taking help for documenting Snow Blitz for this site</p>
             </section>
         <h2>MODDERS</h2>
 
         <section class="homeSubText">
             <p>I hate closed source bullshit, so you can do what you would like. Feel free to create your own fork of Snow Blitz</p>
+            <a href="https://github.com/rugerclaus/snowgame">Snow Blitz Git Repo</a>
             <p>If you want support on the actual game source, I'll be spinning up unknownanarchist.blog's catchall email or something.</p>
         </section>
 
