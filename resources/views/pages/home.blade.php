a2d70d9474e04245680a2e2e4ae964f5c0b4a24b
rugerclaus <rogerrock12games@gmail.com>
2025-05-10 17:30:49 -0600

    micro updates

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,7 +3,10 @@
 @section('main')
     <h1 class="pageHead">Home</h1>
     <section class="homeText">
-        <h2>Page under reconstruction</h2>
-
+        <h2>Contribute to Snow Blitz</h2>
+        <p>Hello and welcome to my website. If you'd like to submit a pull request, please do!</p>
+        <p>You can do a pull request for Snow Blitz or this website but I am only taking help for documenting Snow Blitz for this site</p>
+        <h2>MODDERS</h2>
+        <p>I hate closed source bullshit, so you can do what you would like. Feel free to create your own fork of Snow Blitz</p>
     </section>
 @endsection
